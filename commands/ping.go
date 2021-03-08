551c862dad3c7be598bc8ad2ffdbfdb3b1c6f4eb
root <root@35k9.l.time4vps.cloud>
2021-03-08 14:27:02 +0200

    improved ping command

--- commands/ping.go
+++ commands/ping.go
@@ -2,11 +2,23 @@ package commands
 
 import "../structures"
 import "github.com/bwmarrin/discordgo"
+import "strconv"
 
 var Ping = structures.Command{
     Name: "ping", 
     Description: "A ping command", 
     Run: func (bot*discordgo.Session, msg*discordgo.MessageCreate, args []string) {
-        bot.ChannelMessageSend(msg.ChannelID, "Pong!")
+        m, err := bot.ChannelMessageSend(msg.ChannelID, "Pinging...")
+        
+        if err != nil {
+            return
+        }
+        
+        time1, _ := msg.Timestamp.Parse()
+        time2, _ := m.Timestamp.Parse()
+        
+        time := (time2.UnixNano() - time1.UnixNano()) / 1000000
+        
+        bot.ChannelMessageEdit(msg.ChannelID, m.ID, "Pong! " + strconv.FormatInt(int64(time), 10) + "ms")
     },
 }
